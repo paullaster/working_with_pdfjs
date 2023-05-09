6683c92994cae9fdb9f7f398fbe70f26f1e34aaf
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-05-09 10:09:51 +0300

    pdfjs

--- pdfjs.js
+++ pdfjs.js
@@ -1,3 +1,5 @@
 let pdfLink = "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf";
 
 let pdfjsLib = window["pdfjs-dist/build/pdf"];
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.js";
