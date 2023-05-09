37828754908e9c9ec35741b7814f34620dd9d27b
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-05-09 10:36:38 +0300

    pdfjs

--- pdfjs.js
+++ pdfjs.js
@@ -1,38 +1,43 @@
-let pdfLink = "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf";
+// If absolute URL from the remote server is provided, configure the CORS
+// header on that server.
+var url = 'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf';
 
-let pdfjsLib = window["pdfjs-dist/build/pdf"];
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
 
-pdfjsLib.GlobalWorkerOptions.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.js";
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
 
-let loadingTask = pdfjsLib.getDocument(pdfLink);
-loadingTask.promise.then( (pdf) => {
-    console.log("PDF loaded successfully");
+// Asynchronous download of PDF
+var loadingTask = pdfjsLib.getDocument(url);
+loadingTask.promise.then(function(pdf) {
+  console.log('PDF loaded');
+  
+  // Fetch the first page
+  var pageNumber = 1;
+  pdf.getPage(pageNumber).then(function(page) {
+    console.log('Page loaded');
     
-    // Fetch the first page of pdf
-    let pageNumber = 1;
-    pdf.getPage(pageNumber).then( (page) => {
-        console.log("Page loaded successfully");
-        let scale = 2;
-        let viewport = page.getViewport({scale});
+    var scale = 1.5;
+    var viewport = page.getViewport({scale: scale});
 
-        // Preparing canvas usign pdfjs dimentions
-        let canva = document.getElementById("the-canvas");
-        let context = canva.getContext("2d");
-        canva.height = viewport.height;
-        canva.width = viewport.width;
+    // Prepare canvas using PDF page dimensions
+    var canvas = document.getElementById('the-canvas');
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
 
-        // Render pdf in canva context
-
-        let renderContext = {
-            canvasContet: context,
-            viewport,
-        }
-
-        let renderTask = page.render(renderContext);
-        renderTask.promise.then(() => console.log("Task rendered successfully"));
+    // Render PDF page into canvas context
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+    var renderTask = page.render(renderContext);
+    renderTask.promise.then(function () {
+      console.log('Page rendered');
     });
-},
-(error) => {
-    // PDF Loading Error
-    console.log("PDF Loading Error");
+  });
+}, function (reason) {
+  // PDF loading error
+  console.error(reason);
 });
