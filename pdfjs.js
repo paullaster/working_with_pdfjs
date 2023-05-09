3037d3782958c3323c405101a1cf41e7be18e543
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-05-09 14:24:11 +0300

    pdfjs

--- pdfjs.js
+++ pdfjs.js
@@ -1,15 +1,33 @@
 const initPDF = () => {
   const pdfjsLib = window['pdfjs-dist/build/pdf'];
-  console.log(pdfjsLib)
-// pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
+  pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
 
-// pdfjsLib.getDocument("sample.pdf").promise.then((pdfDoc) => {
-//   console.log(pdfDoc);
-// }).catch((err) => {
-//   console.error(err);
-// });
-
+  pdfjsLib.getDocument("sample2.pdf").promise.then((pdfDoc) => {
+    let pages = pdfDoc._pdfInfo.numPages;
+    for (let i = 0; i < pages; i++) {
+      pdfDoc.getPage(1).then((page) => {
+        const canvas = document.createElement("canvas");
+        const canvasContext = canvas.getContext("2d");
+        const viewPort = page.getViewport({scale: 2});
+        canvas.height = viewPort.height;
+        canvas.width = viewPort.width;
   
+        document.getElementById("pdfViewer").appendChild(canvas);
+        page.render({
+          canvasContext,
+          viewport: viewPort,
+        });
+        console.log(viewPort)
+        console.log(page);
+      }).catch((error) => {
+        console.log(error);
+      });
+    }
+   
+  
+  }).catch((err) => {
+    console.error(err);
+  });
 };
 
 initPDF()
