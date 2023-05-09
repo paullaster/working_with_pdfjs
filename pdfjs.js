5d88e02b8ed8db8157ebda75ff528810a00fbc2d
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-05-09 10:33:41 +0300

    pdfjs

--- pdfjs.js
+++ pdfjs.js
@@ -16,7 +16,23 @@ loadingTask.promise.then( (pdf) => {
         let viewport = page.getViewport({scale});
 
         // Preparing canvas usign pdfjs dimentions
-        let canva = document.getElementById("pdf-canva");
+        let canva = document.getElementById("the-canvas");
         let context = canva.getContext("2d");
-    })
-})
+        canva.height = viewport.height;
+        canva.width = viewport.width;
+
+        // Render pdf in canva context
+
+        let renderContext = {
+            canvasContet: context,
+            viewport,
+        }
+
+        let renderTask = page.render(renderContext);
+        renderTask.promise.then(() => console.log("Task rendered successfully"));
+    });
+},
+(error) => {
+    // PDF Loading Error
+    console.log("PDF Loading Error");
+});
