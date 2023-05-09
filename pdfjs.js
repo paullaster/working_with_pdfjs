2dc14940264925c59227bd00c637d80ed955528a
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-05-09 10:12:49 +0300

    pdfjs

--- pdfjs.js
+++ pdfjs.js
@@ -3,3 +3,12 @@ let pdfLink = "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/example
 let pdfjsLib = window["pdfjs-dist/build/pdf"];
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.js";
+
+let loadingTask = pdfjsLib.getDocument(pdfLink);
+loadingTask.promise.then( (pdf) => {
+    console.log("PDF loaded successfully");
+    
+    // Fetch the first page of pdf
+    let pageNumber = 1;
+    
+})
