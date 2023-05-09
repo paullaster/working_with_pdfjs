0e104f9bdbcabaa4e9f4df513d97c6dd2acec6ce
Paullaster Okoth <paullasterokoth98@gmail.com>
2023-05-09 13:54:08 +0300

    pdfjs

--- pdfjs.js
+++ pdfjs.js
@@ -1,11 +1,15 @@
 const initPDF = () => {
-  const pdfjsLib = window['pdfjs-dist/build/pdf'];
-  pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-  pdfjsLib.getDocument("sample.pdf").promise.then( (pdfDoc) => {
-    console.log(pdfDoc);
-  }).catch((err) => {
-    console.error(err);
-  });
+  // const pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+pdfjsLib.getDocument("sample.pdf").promise.then((pdfDoc) => {
+  console.log(pdfDoc);
+}).catch((err) => {
+  console.error(err);
+});
+
+  
 };
 
 initPDF()
